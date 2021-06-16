423b34d8d94a68c1b5f7608f1aa87477646c9e31
wt1814 <wangtao1814@163.com>
2021-06-16 19:20:40 +0800

    钉钉

--- src/hSearch/aBinarySearch.java
+++ src/hSearch/aBinarySearch.java
@@ -6,6 +6,8 @@ package hSearch;
 public class aBinarySearch {
 
     public static void main(String[] args) {
+        System.out.println(sqrt(4));
+
         int[] array = new int[1000];
         for(int i=0; i<1000;i++){
             array[i] = i;
@@ -14,6 +16,29 @@ public class aBinarySearch {
         System.out.println(binarySearch(array, 173));
     }
 
+    public static int sqrt (int x) {
+        // write code here
+
+        //查找范围起点
+        int start=0;
+        //查找范围终点
+        int end=x;
+        //查找范围中位数
+        int mid;
+
+        while(start<=end){
+            //mid=(start+end)/2 有可能溢出
+            mid=start+(end-start)/2;
+            if(mid * mid == x || ((mid * mid < x) && ((mid+1)*(mid+1)>x))) {
+                return mid;
+            }else if(mid * mid < x){
+                start=mid+1;
+            }else{
+                end=mid-1;
+            }
+        }
+        return -1;
+    }
 
     /**
      * 递归实现
@@ -59,6 +84,8 @@ public class aBinarySearch {
      * @return
      */
     public static int binarySearch(int []array,int target){
+
+
 
         //查找范围起点
         int start=0;
