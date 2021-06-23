fb57038ba03d5b2ba02cdec5352dd0a776c6359f
wt1814 <wangtao1814@163.com>
2021-06-23 23:43:00 +0800

    递归

---
+++ src/jRecursion/prune/generateParenthesis.java
@@ -0,0 +1,50 @@
+package jRecursion.prune;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * 生成括号
+ * todo 递归 + 剪枝 （也可以改写成回溯）
+ */
+public class generateParenthesis {
+
+
+
+    static ArrayList<String> r = new ArrayList<String>();
+
+    public static void main(String[] args) {
+
+        ArrayList<String> strings = generateParenthesis(3);
+        Iterator<String> iterator = strings.iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+    }
+
+
+    // https://www.bilibili.com/video/BV1x5411j7KN/?spm_id_from=333.788.recommend_more_video.1
+
+    public static ArrayList<String> generateParenthesis(int n) {
+        //保证左边‘（’的数量始终大于等于右边的‘）’数量，可以考虑回溯法
+        ArrayList<String> s = new ArrayList<String>();
+        gp("",0,0,n);
+        return r;
+    }
+
+    private static void gp(String s,int left,int right,int n){
+        // 递归终止
+        if(right == n){
+            r.add(s);
+        }
+
+        // 剪枝
+        if (left > n || left < right){
+            return;
+        }
+
+        gp(s+"(",left+1,right,n);
+        gp(s+")",left,right+1,n);
+
+    }
+}
---
+++ src/nLRU/Solution.java
@@ -0,0 +1,73 @@
+package nLRU;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+public class Solution {
+
+
+    public static void main(String[] args) {
+        int[][] a= {{1,1,1},{1,2,2},{1,3,2},{2,1},{1,4,4},{2,2}};
+        LRU(a,3);
+    }
+
+    public LinkedHashMap<Integer,Integer> map;
+    public int K;
+
+    public Solution(){
+
+    }
+
+    public Solution(int K,LinkedHashMap linkedHashMap){
+        this.K = K;
+        this.map = linkedHashMap;
+    }
+
+    public static int[] LRU (int[][] operators, int k) {
+        // write code here
+        Solution solution = new Solution(k,new LinkedHashMap());
+
+        List<Integer> list = new ArrayList();
+        for(int i = 0;i<operators.length;i++){
+            if(operators[i][0] == 1){
+                solution.set(operators[i][1],operators[i][2]);
+            }
+
+            if(operators[i][0] == 2){
+                int num = solution.get(operators[i][1]);
+                list.add(num);
+            }
+        }
+
+        int[] result = new int[list.size()];
+        for(int i = 0;i<list.size();i++){
+            result[i] = list.get(i);
+        }
+        return result;
+    }
+
+    public void set(int key,int value){
+        if(map.containsValue(key)){
+            map.remove(map.get(key));
+            map.put(key,value);
+            return;
+        }
+
+        map.put(key,value);
+        if(map.size()>K){
+            map.remove(map.entrySet().iterator().next().getKey());
+        }
+    }
+
+    public int get(int key){
+        //如果没有找到
+        if (!map.containsKey(key)){
+            return -1;
+        }
+        //找到了就刷新数据
+        Integer value = map.remove(key);
+        map.put(key,value);
+        return value;
+    }
+}
