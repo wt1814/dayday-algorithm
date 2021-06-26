638f78e51010c555b64023aada670eea70a04b35
wt1814 <wangtao1814@163.com>
2021-06-27 00:44:03 +0800

    dp数组

--- src/kDP/number/single/bMaxSubArray.java
+++ src/kDP/number/single/bMaxSubArray.java
@@ -16,6 +16,41 @@ public class bMaxSubArray {
     }
 
 
+    /**
+     * todo 自己写的s
+     * max sum of the subarray
+     * @param arr int整型一维数组 the array
+     * @return int整型
+     */
+    public int maxsumofSubarray (int[] arr) {
+        // write code here
+        if(arr == null || arr.length == 0){
+            return 0;
+        }
+
+        int[] dp = new int[arr.length];
+        dp[0] = arr[0];
+
+        for(int i = 1;i<arr.length;i++){
+            if(dp[i-1]>0){
+                dp[i] = dp[i-1]+arr[i];
+            }else{
+                dp[i] = arr[i];
+            }
+        }
+
+        int result = dp[0];
+        for(int i = 0;i<dp.length;i++){
+            if(dp[i] > result){
+                result = dp[i];
+            }
+        }
+
+        return result;
+    }
+
+    //////////////////////////////////
+
     // https://mp.weixin.qq.com/s/GvBPILaDVhjVhtVDQRLs5g
     // https://leetcode-cn.com/problems/maximum-subarray/
     // https://www.cnblogs.com/dddyyy/p/10746769.html
@@ -45,6 +80,46 @@ public class bMaxSubArray {
             res = Math.max(res, max);
         }
         return res;
+    }
+
+    //////////
+
+    int maxSubArray3(int[] nums) {
+
+
+        if (nums.length == 0){
+            return 0;
+        }
+
+        // dp数组 采用一维数组与入参相似
+
+        // base case
+        int maxSum = nums[0];
+
+
+        for (int i = 1; i < nums.length; ++i) {
+            // max = Math.max(max + array[i], array[i]);
+            nums[i] = Math.max(nums[i - 1], 0) + nums[i]; // todo 状态转移方程
+
+            // dp[i]=max(dp[i-1]+nums[i],nums[i])，其中i>=1，
+            // 当dp[i-1]<0 时，抛弃当前的和最大的连续子数组，从nums[i]开始重新寻找和最大的连续子数组，
+            // 否则将nums[i]加入到当前的和最大的连续子数组。
+            maxSum = Math.max(maxSum, nums[i]);
+        }
+
+        return maxSum;
+    }
+
+
+    /////
+    // leetcode原解
+    public int maxSubArray4(int[] nums) {
+        int pre = 0, maxAns = nums[0];
+        for (int x : nums) {
+            pre = Math.max(pre + x, x);
+            maxAns = Math.max(maxAns, pre);
+        }
+        return maxAns;
     }
 
 
