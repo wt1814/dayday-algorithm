272bbf7afeb7eae4fb0915f6e351dd3d46e1929e
wt1814 <wangtao1814@163.com>
2021-08-03 16:19:29 +0800

    nihao

--- src/oDP/dMatrix/MinPathSum.java
+++ src/oDP/dMatrix/MinPathSum.java
@@ -23,6 +23,8 @@ public class MinPathSum {
      */
     public static int minPathSum (int[][] matrix) {
         int[][]  dp = new int[matrix.length][matrix[0].length];
+
+        //
         dp[0][0] = matrix[0][0];
         for(int i =1; i< matrix.length; i++) {
             dp[i][0] =  dp[i-1][0] + matrix[i][0];
@@ -31,6 +33,7 @@ public class MinPathSum {
             dp[0][i] =  dp[0][i-1] + matrix[0][i];
         }
 
+        //
         for (int i = 1; i < dp.length; i++) {
             for (int j = 1; j < dp[0].length; j++) {
                 dp[i][j] = matrix[i][j] + Math.min(dp[i-1][j], dp[i][j-1]);
