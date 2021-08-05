19890b801f3a956eeaa86b1318a72b564882cd2e
wt1814 <wangtao1814@163.com>
2021-08-05 22:40:26 +0800

    矩阵

---
+++ src/aTest/day0805/test2.java
@@ -0,0 +1,49 @@
+package aTest.day0805;
+
+import dLinked.ListNode;
+
+public class test2 {
+
+
+    public static void main(String[] args){
+        ListNode node1 = new ListNode(5);
+        ListNode node2 = new ListNode(4,node1);
+        ListNode node3 = new ListNode(3,node2);
+        ListNode node4 = new ListNode(2,node3);
+        ListNode node5 = new ListNode(1,node4);
+
+        ListNode listNode = reverseBetween(node5, 2, 4);
+        System.out.println(listNode.val);
+    }
+
+    public static ListNode reverseBetween (ListNode head, int m, int n) {
+        // write code here
+        ListNode dummyNode = new ListNode(-1);
+        dummyNode.next = head;
+        ListNode preNode = dummyNode;
+
+        int num = 1;
+        while(num <m){
+            preNode = preNode.next;
+            num++;
+        }
+
+        ListNode tempHead = preNode.next;
+        ListNode cur = tempHead.next;
+        ListNode next = null;
+        while(num <=n){
+            next = cur.next;
+            cur.next = tempHead;
+            tempHead = cur;
+            cur = next;
+            preNode.next = tempHead;
+            num++;
+        }
+        System.out.println(tempHead.val);
+        System.out.println(cur.val);
+        tempHead.next = cur;
+        preNode.next = tempHead;
+        return dummyNode.next;
+
+    }
+}
--- src/dLinked/dReverseKGroup.java
+++ src/dLinked/dReverseKGroup.java
@@ -8,4 +8,40 @@ public class dReverseKGroup {
     // https://www.nowcoder.com/practice/b49c3dc907814e9bbfa8437c251b028e?tpId=188&&tqId=38557&rp=1&ru=/activity/oj&qru=/ta/job-code-high-week/question-ranking
 
 
+    /**
+     *
+     * @param head
+     * @param k
+     * @return
+     */
+    public static ListNode reverseKGroup (ListNode head, int k) {
+        if(head==null||head.next==null||k==1) return head;
+        ListNode res = new ListNode(0);
+        res.next = head;
+        int length = 0;
+        ListNode pre = res,
+                cur = head,
+                temp = null;
+        while(head!=null){
+            length++;
+            head = head.next;
+        }
+        //分段使用头插法将链表反序
+        for(int i=0; i<length/k; i++){
+            //pre作为每一小段链表的头节点，负责衔接
+            for(int j=1; j<k; j++){
+                temp = cur.next;
+                cur.next = temp.next;
+                //相当于头插法，注意：
+                //temp.next = cur是错误的，temp需要连接的不是前一节点，而是子序列的头节点
+                temp.next = pre.next;
+                pre.next = temp;
+            }
+            //每个子序列反序完成后，pre，cur需要更新至下一子序列的头部
+            pre = cur;
+            cur = cur.next;
+        }
+        return res.next;
+    }
+
 }
