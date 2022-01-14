2fc42586f65e30b95080e3b5bf8504e8621be76d
wangtao1814source <wangtao181402@163.com>
2022-01-14 16:44:15 +0800

    位运算

---
+++ src/rBitOperation/aSingleNumber.java
@@ -0,0 +1,26 @@
+package rBitOperation;
+
+/**
+ * 136. 只出现一次的数字
+ * 链接：https://leetcode-cn.com/problems/single-number
+ * 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+ *
+ * 说明：
+ * 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+ *
+ * 示例 1:
+ * 输入: [2,2,1]
+ * 输出: 1
+ *
+ * 示例 2:
+ * 输入: [4,1,2,1,2]
+ * 输出: 4
+ */
+public class aSingleNumber {
+
+    // https://mp.weixin.qq.com/mp/appmsgalbum?__biz=Mzk0NzIxMDgwNA==&action=getalbum&album_id=1990389635625091072&scene=173&from_msgid=2247484607&from_itemidx=1&count=3&nolastread=1#wechat_redirect
+
+    // https://mp.weixin.qq.com/s/jQAi8gpctRR5V6gahFnHpQ
+
+
+}
